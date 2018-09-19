e244de78953e9f76aa4dc511c30860bda5ce21ae
Luke <39895263+LukeWells@users.noreply.github.com>
2018-09-19 15:34:33 +1000

    CSS updates
    better readability

--- display.php
+++ display.php
@@ -6,7 +6,8 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Jig sheet viewer</title>
 </head>
-<body>
+<body>	
+	<div class="container">
 	<?php
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
@@ -25,15 +26,16 @@
 			 
 			if($jigList != "outputs") {
 			
-				echo "<a href='jigs.php?folderName=".$folderName."&amp;poolId=".$poolId."'>";
+				echo "<a class='btn btn-default btn-lg' href='jigs.php?folderName=".$folderName."&amp;poolId=".$poolId."'>";
 				echo $jigList;
-				echo "</a>";
+				echo "</a><br/>";
 				
 			}
 		}
 		echo "<br>";
 	}
     ?>
+	</div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -7,17 +7,21 @@
     <title>Jig sheet viewer</title>
 </head>
 <body>
+	<div class="container">
 	<?php
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
 	$jigPath = "D:/jigFolder/";
+
+	echo "<table cellspacing='200'><tr><th>Pool ID</th><th>Last Modified Date</th></tr><br/>";
 	
 	$dir = new DirectoryIterator($jigPath);
 	foreach ($dir as $fileinfo) {
 		if (!$fileinfo->isDot()) {
 			$jigList = $fileinfo->getFilename();
+			$modTime = $fileinfo->getMTime();
 			$info = pathinfo($jigList);
 			$folderName = $info['filename'];
 			$curPath = $jigPath.$folderName;
@@ -31,17 +35,19 @@
 			}
 			 
 			if($jigList != "outputs") {
-			
-				echo "<a href='display.php?folderName=".$folderName."&amp;poolId=".$poolId."'>";
+				
+				echo "<tr><td>";
+				echo "<a class='btn btn-default btn-lg' href='display.php?folderName=".$folderName."&amp;poolId=".$poolId."'>";
 				echo $jigList;
-				echo "</a>";
-			
+				echo "</a></td><td>";
+				echo "&nbsp;".date('r',$modTime);
+				echo "</td></tr>";
 			}
 		}
-		echo "<br>";
 	}
 	
     ?>
+	</div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
